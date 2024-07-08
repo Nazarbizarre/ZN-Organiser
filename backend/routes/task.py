bcba1e87b0ddbb2c215bea077dac81bcca872a90
_G0o0se_ <154550493+Nazarbizarre@users.noreply.github.com>
2024-07-08 16:33:17 +0200

    Revert "Update frontend"

--- backend/routes/task.py
+++ backend/routes/task.py
@@ -2,15 +2,15 @@ from main import app
 from sqlalchemy import select
 from datetime import datetime
 from db import Session, Task
-from schemas import TaskData, UserTasks
+from schemas import TaskData
 
 
 # from fastapi.exceptions import HTTPException
 
 @app.get("/get_tasks")
-def get_tasks(data: UserTasks):
+def get_tasks():
     with Session.begin() as session:
-        tasks = session.scalars(select(Task).where(Task.author == data.email)).all()
+        tasks = session.scalars(select(Task)).all()
         tasks = [TaskData.model_validate(task) for task in tasks]
         return tasks
 
--- backend/schemas/task.py
+++ backend/schemas/task.py
@@ -6,11 +6,9 @@ from datetime import datetime
 class TaskData(BaseModel):
     model_config = ConfigDict(from_attributes=True)
     id: int
-    author: str
+
     title: str
     content: str
     published: datetime
     deadline: datetime
 
-class UserTasks(BaseModel):
-    email: str
