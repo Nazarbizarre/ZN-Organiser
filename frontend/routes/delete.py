6fb5479dc4816a6bc574d3ee58c107ee573004d3
prerreslin <sanyatop011209@gmail.com>
2024-09-06 11:45:10 +0300

    Changes to be committed: 	modified:   backend/task.db 	modified:   frontend/routes/completed.py 	modified:   frontend/routes/delete.py 	new file:   frontend/templates/completed.html 	modified:   frontend/templates/info.html 	modified:   frontend/templates/main.html

--- frontend/routes/delete.py
+++ frontend/routes/delete.py
@@ -1,6 +1,8 @@
 from .. import app,BACKEND_URL
 from flask import render_template, redirect, url_for,request
 from requests import delete
+from flask_login import current_user
+from requests import get
 
 @app.get("/delete_task/<int:task_id>")
 def delete_validator(task_id):
@@ -9,11 +11,17 @@ def delete_validator(task_id):
 @app.post("/delete_task/<int:task_id>")
 def delete_task(task_id):
     response = request.form.get("response")
+    tasks_response = get(f"{BACKEND_URL}/task/{task_id}")
+    redirect_pos = "index"
+    if tasks_response.status_code == 200:
+        selected_product = tasks_response.json()
+        if selected_product.get("completed") == True:
+            redirect_pos = "completed"
     if response == "yes":
         print("yes")
-        response = delete(f"{BACKEND_URL}/delete_task", json={"id": task_id})
+        response = delete(f"{BACKEND_URL}/delete_task", json={"id": task_id,"user": current_user.email})
         if response.status_code == 200:
-            return redirect(url_for("index"))
+            return redirect(url_for(redirect_pos))
         else:
             return f"Error {response.status_code}"
     if response == "no":
