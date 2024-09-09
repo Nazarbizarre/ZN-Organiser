7bb184571b5a2cf2a49fafa4fa9f58e7533637c7
Bohdan Savin <bogdan07112009@gmail.com>
2024-09-09 11:28:19 +0300

    Task pos | Refactor after Nazar and Lenya

--- frontend/routes/search.py
+++ frontend/routes/search.py
@@ -13,10 +13,11 @@ def search():
     if query:
         email = current_user.email
         data = {
-            "email": email
+            "email": email,
+            "completed": False
         }
         tasks = get("http://127.0.0.1:8000/get_tasks", json=data).json()
-        filtered = [task for task in tasks if query.lower() in task['title'].lower()]
+        filtered = [task for task in tasks if query.lower() in task.get('title').lower()]
         nickname = current_user.email.split("@")[0]
         return render_template("main.html", tasks=filtered, query=query, nickname=nickname)
     else:
